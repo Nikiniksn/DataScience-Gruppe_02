aac840460535d6b7547ce19c816fdc2b7441a3f2
sven schadwald <sven.schadwald@googlemail.com>
2020-05-12 19:40:54 +0200

    Import Umsatzdaten

--- script.R
+++ script.R
@@ -1,11 +1,9 @@
 #hier geht es bald los
-<<<<<<< HEAD
-library(readr)
-umsatzdaten_gekuerzt <- read_csv("umsatzdaten_gekuerzt.csv")
-View(umsatzdaten_gekuerzt)
-=======
 #import kiwo data
 library(readr)
 kiwo <- read_csv("kiwo.csv")
 View(kiwo)
->>>>>>> 14e60aa255cb3c0120695f89081d2f8495d41772
+#import umsatzdaten
+library(readr)
+umsatzdaten_gekuerzt <- read_csv("umsatzdaten_gekuerzt.csv")
+View(umsatzdaten_gekuerzt)
