504df8cdac3194e746d3bc56c2b0d1f0e396279e
sven schadwald <sven.schadwald@googlemail.com>
2020-05-17 11:52:14 +0200

    Die Feritage und das Wetter wurde ergänzt

--- script.R
+++ script.R
@@ -1,12 +1,23 @@
 #hier geht es bald los
-#import kiwo data
+#Packages
 library(readr)
+
+#import kiwo data
 kiwo <- read_csv("kiwo.csv")
 View(kiwo)
+
 #import umsatzdaten
-library(readr)
 umsatzdaten_gekuerzt <- read_csv("umsatzdaten_gekuerzt.csv")
 View(umsatzdaten_gekuerzt)
+
 #import schulferien
 schulferien <- read_csv("Schulferien.csv")
 View(schulferien)
+
+#import Feiertage
+feiertage <- read_csv("Feiertage.csv")
+View(feiertage)
+
+#import Wetter
+wetter <- read_csv("wetter.csv")
+View(wetter)
